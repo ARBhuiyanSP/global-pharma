1b48c6adb5061c9e8dc6ae69fdbabeb7aa31248b
unknown <arbhuiyan.pits@gmail.com>
2023-04-10 09:09:29 +0600

    first commit

--- DATABASE FILE/global_pharma.sql
+++ DATABASE FILE/global_pharma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2023 at 06:41 AM
+-- Generation Time: Apr 10, 2023 at 05:08 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
